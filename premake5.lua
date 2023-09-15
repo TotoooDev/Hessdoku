dd8d65f8139e72bc18e0b96e3bf8504734f1d9d6
Tom <91008122+TotoooDev@users.noreply.github.com>
2023-09-15 15:57:42 +0200

    bam

---
+++ premake5.lua
@@ -0,0 +1,64 @@
+-- Stolen from Cherno's video on premake
+-- https://www.youtube.com/watch?v=sULV3aB2qeU
+
+-- Defines the name of the workspace (the Visual Studio .sln file)
+workspace "Hessdoku"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+	startproject "Hessdoku"
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Example project
+project "Hessdoku"
+	location "Hessdoku"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputDir .. "/%{prj.name}")
+
+	debugdir ("bin/" .. outputDir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.c"
+	}
+	
+	includedirs
+	{
+		"Hessdoku/src",
+		"Hessdoku/src/vendor"
+	}
+	
+	filter "system:windows"
+		staticruntime "off"
+		runtime "Release"
+		systemversion "latest"
+		libdirs ("libs/windows")
+		-- links ()
+		postbuildcommands ("xcopy ..\\libs\\windows\\dlls\\ ..\\bin\\" .. outputDir .. "\\%{prj.name}\\ /s /e /y /i")
+		postbuildcommands ("xcopy ..\\dev-assets\\ ..\\bin\\" .. outputDir .. "\\%{prj.name}\\ /s /e /y /i")
+
+	filter "system:linux"
+		staticruntime "off"
+		systemversion "latest"
+		libdirs ("libs/linux")
+
+	filter "configurations:Debug"
+		defines
+		{
+			"SUDOKU_DEBUG"
+		}
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"

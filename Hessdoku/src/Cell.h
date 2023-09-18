3263d3823acf78ac7f2921a2374fc925052d7fd5
TotoooDev <totooodev@gmail.com>
2023-09-18 11:20:15 +0200

    added cool stuff

--- Hessdoku/src/Cell.h
+++ Hessdoku/src/Cell.h
@@ -1,9 +1,15 @@
 #ifndef CELL_H
 #define CELL_H
 
+/**
+ * A simple type representing a cell of the sudoku grid.
+*/
 typedef struct T_Cell
 {
-
+    /**
+     * This is the value stored in the cell, defined in 4 bits.
+    */
+    unsigned char value : 4;
 } T_Cell;
 
 #endif
--- Hessdoku/src/Main.c
+++ Hessdoku/src/Main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <Cell.h>
 
 int main(int argc, char* argv[])
 {

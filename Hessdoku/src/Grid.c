455ef7c6fcc8659e759bbd1228df2d953d0c0b76
TotoooDev <totooodev@gmail.com>
2023-09-18 11:56:29 +0200

    Added addCell and removeCell

--- Hessdoku/src/Grid.c
+++ Hessdoku/src/Grid.c
@@ -1,2 +1,31 @@
 #include <Grid.h>
+#include <Log.h>
 
+void addCell(T_Grid grid, unsigned char x, unsigned char y, T_Cell* cell)
+{
+    if (!cell)
+    {
+        LOG("Trying to add a NULL cell at %d,%d!\n", x, y);
+        LOG("If you want to remove a cell, use removeCell instead.\n");
+        return;
+    }
+
+    if (grid[x][y])
+    {
+        LOG("A cell already exists at %d,%d!", x, y);
+        return;
+    }
+
+    grid[x][y] = cell;
+}
+
+void removeCell(T_Grid grid, unsigned char x, unsigned char y)
+{
+    if (!grid[x][y])
+    {
+        LOG("Trying to remove a cell a %d,%d but there was no cell there!", x, y);
+        return;
+    }
+
+    grid[x][y] = NULL;
+}
--- Hessdoku/src/Grid.h
+++ Hessdoku/src/Grid.h
@@ -3,6 +3,29 @@
 
 #include <Cell.h>
 
-typedef T_Cell** T_Grid;
+typedef T_Cell*** T_Grid;
+
+/**
+ * Add a cell into the grid.
+ *
+ * @param grid The grid in which the cell will be added
+ * @param x The x position of the new cell
+ * @param y The y position of the new cell
+ * @param cell The cell to be added
+ *
+ * @author Toto
+ */
+void addCell(T_Grid grid, unsigned char x, unsigned char y, T_Cell* cell);
+
+/**
+ * Remove a cell from the grid. This does not free the cell.
+ *
+ * @param grid The grid the cell will be removed from
+ * @param x The x position of the cell
+ * @param y The y position of the cell
+ *
+ * @author Toto
+ */
+void removeCell(T_Grid grid, unsigned char x, unsigned char y);
 
 #endif

b4711173b0dbd0bd9f15e78c08f9bf691822de25
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2023-09-18 11:46:16 +0200

    Update Cell.h
    Added T_Cell definition

--- Hessdoku/src/Cell.h
+++ Hessdoku/src/Cell.h
@@ -1,12 +1,15 @@
 #ifndef CELL_H
 #define CELL_H
 
+#include <stdbool.h>
+
 /**
  * A simple type representing a cell of the sudoku grid.
  */
 typedef struct T_Cell
 {
-    
+    bool* notes;
+    unsigned char value : 4;
 } T_Cell;
 
 #endif

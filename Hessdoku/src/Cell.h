cb7856399cf1c9b92918ebc88a43c10f9ea9649d
TotoooDev <totooodev@gmail.com>
2023-09-18 11:22:04 +0200

    cool indentation

--- Hessdoku/src/Cell.h
+++ Hessdoku/src/Cell.h
@@ -3,12 +3,12 @@
 
 /**
  * A simple type representing a cell of the sudoku grid.
-*/
+ */
 typedef struct T_Cell
 {
     /**
      * This is the value stored in the cell, defined in 4 bits.
-    */
+     */
     unsigned char value : 4;
 } T_Cell;
 

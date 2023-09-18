dd1e4029173891db7bcbfc71f5fa54cfced85de2
Vosk <34106377+vosketalor@users.noreply.github.com>
2023-09-18 11:42:33 +0200

    Adding a cool display function (untested tho)
    🤞

--- Hessdoku/src/Grid.c
+++ Hessdoku/src/Grid.c
@@ -1,2 +1,52 @@
 #include <Grid.h>
 
+
+/**
+ * Displays the given grid in a verry pretty way in the console.
+ *
+ * @param grid The grid to display
+ * @param size The size of the grid (square)
+ * @author Baptiste
+ */
+void displayGridToConsole(T_Grid grid, int size) {
+
+    for(int i = 0; i < size; i++) 
+    {
+        if(i%3 == 0) 
+        {
+            for(int k = 0; k < size; k++) printf("####");
+            printf("#\n");
+        }
+
+        for(int k = 0; k < size; k++) {
+            printf(k%3 == 0 ? "#" : "|");
+
+            if(grid[i][k].value == 0) 
+            {
+                printf("   ");
+            } 
+            else 
+            {
+                printf(" %hu ", grid[i][k].value);
+            }
+
+        }
+        printf("#\n");
+
+        for(int k = 0; k < size && i%3!=2; k++) 
+        {
+            printf(k%3 == 0 ? "#" : "+");
+            printf("---");
+
+        }
+        if(i%3!=2) printf("#\n");
+    }
+
+    // Ending line
+    for(int k = 0; k < size; k++) 
+    {
+        printf("####");
+    }
+    printf("#");
+
+}

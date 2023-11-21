3c1ff8f560e591f1b4cfd051e0092765a75e8978
Tom <totooodev@gmail.com>
2023-11-21 11:26:00 +0100

    Removed unused code

--- Hessdoku/src/Solver.h
+++ Hessdoku/src/Solver.h
@@ -108,41 +108,4 @@ bool gridVerif(T_Grid grid);
  */
 bool removeNotesInGridByZones(T_Grid grid);
 
-
-//PREVIOUS CODE
-
-
-/**
- * Reset a table to false
- * 
- * @param w : a table of 9 booleans
- * 
- * @return w with all his value at false
- * 
- * @author Marie
- */
-//void resetWitness(bool *w);
-
-
-/**
-* Display a table of 9 booleans
-* 
-* @param w : a table of 9 booleans
-* 
-* @author Marie
-*/
-//void aff_witness(bool* w);
-
-
-/**
-* Check if the sudoku is correct
-* 
-* @param grid : the sudoku
-* 
-* @returns true (1) : no error ; false (0) : error
-* 
-* @author Marie
-*/
-//bool checkingAccuracy(T_Grid grid);
-
 #endif

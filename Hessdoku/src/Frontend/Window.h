8df8272b328ee45a1449681eb27e7ab7338145d8
TotoooDev <totooodev@gmail.com>
2024-01-08 16:24:55 +0100

    Added drawing functions

--- Hessdoku/src/Frontend/Window.h
+++ Hessdoku/src/Frontend/Window.h
@@ -1,6 +1,7 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include <Grid.h>
 #include <stdbool.h>
 
 /**
@@ -45,13 +46,46 @@ void clearWindow(T_Window* window, unsigned char r, unsigned char g, unsigned ch
 void presentWindow(T_Window* window);
 
 /**
- * Returns whether the window is open or not. What can close a window include inputs like:
- * - Clicking the x button.
- * - Pressing Alt-F4.
- * - And probably other things.
+ * Returns whether the window is open or not. What can close a window include inputs like clicking the x button, ressing Alt-F4, and probably other things.
  * @param window The window to check for.
  * @return `true` if the window is open, `false` otherwise.
 */
 bool isWindowOpen(T_Window* window);
+
+/**
+ * Sets the draw color for the next draw calls.
+ * @param window The window to set the draw color to.
+ * @param r The red component of the clear color (0-255).
+ * @param g The blue component of the clear color (0-255).
+ * @param b The green component of the clear color (0-255).
+ */
+void setDrawColor(T_Window* window, unsigned char r, unsigned char g, unsigned char b);
+
+/**
+ * Draws a line.
+ * @param window The window to draw the line to.
+ * @param startX The x starting point of the line.
+ * @param startY The y starting point of the line.
+ * @param endX The x starting point of the line.
+ * @param endY The y ending point of the line.
+*/
+void drawLine(T_Window* window, int startX, int startY, int endX, int endY);
+
+/**
+ * Draws a rectangle shape.
+ * @param window The window to draw the line to.
+ * @param x The x starting point of the rect.
+ * @param y The y starting point of the rect.
+ * @param width The width of the rect.
+ * @param height The height of the shape.
+*/
+void drawRect(T_Window* window, int x, int y, int width, int height);
+
+/**
+ * Draws a whole grid.
+ * @param window The window to draw the line to.
+ * @param grid The grid to draw.
+*/
+void drawGrid(T_Window* window, T_Grid grid);
 
 #endif

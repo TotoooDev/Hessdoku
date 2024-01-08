1e593fdb2a359e4200f4f5fc885ea14e7064b126
TotoooDev <totooodev@gmail.com>
2024-01-08 16:09:08 +0100

    Added frontend doc

--- Hessdoku/src/Frontend/Frontend.h
+++ Hessdoku/src/Frontend/Frontend.h
@@ -3,10 +3,28 @@
 
 #include <Game.h>
 
+/**
+ * A frontend structure that holds everything related to the graphical interface.
+ */
 typedef struct T_Frontend T_Frontend;
 
+/**
+ * Creates a frontend structure
+ * @param game The game to display to the frontend.
+ * @return The new frontend.
+*/
 T_Frontend* createFrontend(T_Game* game);
+
+/**
+ * Run the frontend. This executes the main loop of the frontend.
+ * @param frontend The frontend to run.
+ */
 void runFrontend(T_Frontend* frontend);
+
+/**
+ * Free a frontend. The game associated to the frontend is NOT freed, and it is the responsability of the backend to free the memory.
+ * @param frontend The frontend to free.
+ */
 void freeFrontend(T_Frontend* frontend);
 
 #endif

fa91d79aa521a31c6afab424d0f70aa9855cf4b2
TotoooDev <totooodev@gmail.com>
2024-01-11 15:35:32 +0100

    Added T_Color

---
+++ Hessdoku/src/Frontend/Color.h
@@ -0,0 +1,12 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+/**
+ * A structure to represent an RGB8 color (ranging 0-255).
+ */
+typedef struct T_Color
+{
+    unsigned char r, g, b;
+} T_Color;
+
+#endif

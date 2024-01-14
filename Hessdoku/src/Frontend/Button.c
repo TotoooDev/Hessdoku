ab6df511d1f2e6890d024134af921ee1829cdf26
TotoooDev <totooodev@proton.me>
2024-01-14 14:01:37 +0100

    Added buttons

---
+++ Hessdoku/src/Frontend/Button.c
@@ -0,0 +1,65 @@
+#include <Frontend/Button.h>
+#include <stdlib.h>
+
+typedef struct T_Button
+{
+    int x, y; /** The coordiantes of the button */
+    int width, height; /** The size of the button */
+    T_ButtonFunction function; /** The function pointer that executes the code when the button is pressed */
+} T_Button;
+
+T_Button* createButton(int x, int y, int width, int height, T_ButtonFunction function)
+{
+    T_Button* button = (T_Button*)malloc(sizeof(T_Button));
+
+    button->x = x;
+    button->y = y;
+    button->width = width;
+    button->height = height;
+    button->function = function;
+
+    return button;
+}
+
+void freeButton(T_Button* button)
+{
+    free(button);
+}
+
+void setButtonCoordinates(T_Button* button, int x, int y)
+{
+    button->x = x;
+    button->y = y;
+}
+
+void setButtonSize(T_Button* button, int width, int height)
+{
+    button->width = width;
+    button->height = height;
+}
+
+void setButtonFunction(T_Button* button, T_ButtonFunction function)
+{
+    button->function = function;
+}
+
+void getButtonCoordinates(T_Button* button, int* x, int* y)
+{
+    if (x != NULL)
+        *x = button->x;
+    if (y != NULL)
+        *y = button->y;
+}
+
+void getButtonSize(T_Button* button, int* width, int* height)
+{
+    if (width != NULL)
+        *width = button->width;
+    if (height != NULL)
+        *height = button->height;
+}
+
+T_ButtonFunction getButtonFunction(T_Button* button)
+{
+    return button->function;
+}

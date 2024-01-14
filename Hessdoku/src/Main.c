786273dce464eb286fe7802c40be20ee6be84244
TotoooDev <totooodev@proton.me>
2024-01-14 12:57:16 +0100

    Added grid mods

--- Hessdoku/src/Main.c
+++ Hessdoku/src/Main.c
@@ -4,6 +4,10 @@
 int main(int argc, char* argv[])
 {
     T_Grid grid = generateGrid(81, 9);
+    setCell(grid, 0, 0, 1);
+    setCell(grid, 1, 0, 2);
+    unsetNoteCell(getCell(grid, 2, 0), 1);
+    unsetNoteCell(getCell(grid, 3, 0), 5);
     T_Frontend* frontend = createFrontend(grid);
 
     runFrontend(frontend);

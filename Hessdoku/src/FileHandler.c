4318149c3af299b94d3f10cc1c28927d746c134f
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2024-01-22 11:18:32 +0100

    Added comments to the file handler

--- Hessdoku/src/FileHandler.c
+++ Hessdoku/src/FileHandler.c
@@ -4,40 +4,57 @@
 
 #include "fileHandler.h";
 
+
+/**
+* FILE HANDLER
+* 
+* This library allows to read a file character by character.
+* When a file is open, we place a "cursor" on the first element of the file.
+* Then, when the next character is read, the cursor will move past it.
+* 
+* Considering a FILE, this library will allow to read it easyly, by either advancing the 
+* cursor until a given character or read the numbers until a character.
+*/
+
+
 FILE* openFile(char * name)
 {
 	FILE* fd = fopen(name, "r");
-	assert(fd != NULL);
+	assert(fd != NULL);				// Asserts that the file has been opened
 	return fd;
 }
 
 void closeFile(FILE* file)
 {
 	int res = fclose(file);
-	assert(res == 0);
+	assert(res == 0);				// Asserts that the file has been closed
 }
 
-// TODO: doc dans le .h
-// TODO: handle infinite loop cases
 void skipUntil(FILE* fd, char c, int occurences)
 {
+	const int maxRepetitions = 1000;
+
 	char characterRead = ' '; 
-	char repetitions = 0;
+	int repetitions = 0;		// Infinite loop protection : if code iterates over maxRepetitions times without changes the file is incorrect
+	
+	// Looping until either c is found or the loop is too long
 	while (occurences != 0)
 	{
 		fread(&characterRead, sizeof(char), 1, fd);
 		if (characterRead == c)
 		{
 			occurences--;
+			repetitions = 0;
 		} 
 		
+		// If ' is read, we are at the end of the file !
 		else if (characterRead == '\'')
 		{
 			occurences = 0;
 		}
 
 		repetitions++;
-		if (repetitions == 1000)
+		if (repetitions == maxRepetitions)
 		{
 			printf("Attention, le formatage du fichier est mauvais !  (attente)");
 			exit(EXIT_FAILURE);
@@ -45,13 +62,15 @@ void skipUntil(FILE* fd, char c, int occurences)
 	}
 }
 
-/* fread version*/
 unsigned int readNumbersUntil(FILE* fd, char c)
 {
+	const int maxRepetitions = 1000;
+
 	char characterRead = ' ';
 	int nb = 0;
 	int repetitions = 0;
 
+	// Looping until either c is found or the loop is too long
 	while (characterRead != c)
 	{
 		unsigned int size = fread(&characterRead, sizeof(char), 1, fd);
@@ -63,7 +82,7 @@ unsigned int readNumbersUntil(FILE* fd, char c)
 		}
 
 		repetitions++;
-		if (repetitions == 10000)
+		if (repetitions == maxRepetitions)
 		{
 			printf("Attention, le formatage du fichier est mauvais ! (lecture d'un nombre)");
 			exit(EXIT_FAILURE);
@@ -72,17 +91,3 @@ unsigned int readNumbersUntil(FILE* fd, char c)
 
 	return nb;
 }
-
-/*
-char readNext(FILE* fd, char* ignore)
-{
-	while ()
-}
-*/
-
-/* A faire pour la bibliotheque et dans grid
-
-  - une fonction readNext, qui permet de lire le prochain caractère en ignorant tous les caractères du tableau ignore.
-  - Il faudra peut-être une fonction qui lit jusqu'à un certain élément (en l'occurence, "|") et, si elle trouve une valeur elle la renvoie sinon elle renvoie 0 ou un truc comme ça
-  - une fois cela fait, l'utiliser dans une boucle infinie pour générer le tableau
-*/

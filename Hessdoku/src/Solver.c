35c23e7f6546a217a17f2c2fc908965af98c948b
Vosk <34106377+vosketalor@users.noreply.github.com>
2024-01-22 10:55:14 +0100

    Updated ktuples generation

--- Hessdoku/src/Solver.c
+++ Hessdoku/src/Solver.c
@@ -420,84 +420,28 @@ bool removeNoteOnCell(T_Grid grid, int** cooTuple, int* tuple, int k)
 	return hasChanged;
 }
 
-int** generateKtuples(int tupleSize, int nbCombinations)
-{
-	if (tupleSize < 0)
-	{
-		fprintf(stderr, "Invalid tuple size given (%d)\n", tupleSize);
+int tuplesSize1[][1] = { {1},{2},{3},{4},{5},{6},{7},{8},{9} };
+int tuplesSize2[][2] = { {1,2},{1,3},{1,4},{1,5},{1,6},{1,7},{1,8},{1,9},{2,3},{2,4},{2,5},{2,6},{2,7},{2,8},{2,9},{3,4},{3,5},{3,6},{3,7},{3,8},{3,9},{4,5},{4,6},{4,7},{4,8},{4,9},{5,6},{5,7},{5,8},{5,9},{6,7},{6,8},{6,9},{7,8},{7,9},{8,9} };
+int tuplesSize3[][3] = { {1,2,3},{1,2,4},{1,2,5},{1,2,6},{1,2,7},{1,2,8},{1,2,9},{1,3,4},{1,3,5},{1,3,6},{1,3,7},{1,3,8},{1,3,9},{1,4,5},{1,4,6},{1,4,7},{1,4,8},{1,4,9},{1,5,6},{1,5,7},{1,5,8},{1,5,9},{1,6,7},{1,6,8},{1,6,9},{1,7,8},{1,7,9},{1,8,9},{2,3,4},{2,3,5},{2,3,6},{2,3,7},{2,3,8},{2,3,9},{2,4,5},{2,4,6},{2,4,7},{2,4,8},{2,4,9},{2,5,6},{2,5,7},{2,5,8},{2,5,9},{2,6,7},{2,6,8},{2,6,9},{2,7,8},{2,7,9},{2,8,9},{3,4,5},{3,4,6},{3,4,7},{3,4,8},{3,4,9},{3,5,6},{3,5,7},{3,5,8},{3,5,9},{3,6,7},{3,6,8},{3,6,9},{3,7,8},{3,7,9},{3,8,9},{4,5,6},{4,5,7},{4,5,8},{4,5,9},{4,6,7},{4,6,8},{4,6,9},{4,7,8},{4,7,9},{4,8,9},{5,6,7},{5,6,8},{5,6,9},{5,7,8},{5,7,9},{5,8,9},{6,7,8},{6,7,9},{6,8,9},{7,8,9} };
+int tuplesSize4[][4] = { {1,2,3,4}, {1,2,3,5},{1,2,3,6},{1,2,3,7},{1,2,3,8},{1,2,3,9},{1,2,4,5},{1,2,4,6},{1,2,4,7},{1,2,4,8},{1,2,4,9},{1,2,5,6},{1,2,5,7},{1,2,5,8},{1,2,5,9},{1,2,6,7},{1,2,6,8},{1,2,6,9},{1,2,7,8},{1,2,7,9},{1,2,8,9},{1,3,4,5},{1,3,4,6},{1,3,4,7},{1,3,4,8},{1,3,4,9},{1,3,5,6},{1,3,5,7},{1,3,5,8},{1,3,5,9},{1,3,6,7},{1,3,6,8},{1,3,6,9},{1,3,7,8},{1,3,7,9},{1,3,8,9},{1,4,5,6},{1,4,5,7},{1,4,5,8},{1,4,5,9},{1,4,6,7},{1,4,6,8},{1,4,6,9},{1,4,7,8},{1,4,7,9},{1,4,8,9},{1,5,6,7},{1,5,6,8},{1,5,6,9},{1,5,7,8},{1,5,7,9},{1,5,8,9},{1,6,7,8},{1,6,7,9},{1,6,8,9},{1,7,8,9},{2,3,4,5},{2,3,4,6},{2,3,4,7},{2,3,4,8},{2,3,4,9},{2,3,5,6},{2,3,5,7},{2,3,5,8},{2,3,5,9},{2,3,6,7},{2,3,6,8},{2,3,6,9},{2,3,7,8},{2,3,7,9},{2,3,8,9},{2,4,5,6},{2,4,5,7},{2,4,5,8},{2,4,5,9},{2,4,6,7},{2,4,6,8},{2,4,6,9},{2,4,7,8},{2,4,7,9},{2,4,8,9},{2,5,6,7},{2,5,6,8},{2,5,6,9},{2,5,7,8},{2,5,7,9},{2,5,8,9},{2,6,7,8},{2,6,7,9},{2,6,8,9},{2,7,8,9},{3,4,5,6},{3,4,5,7},{3,4,5,8},{3,4,5,9},{3,4,6,7},{3,4,6,8},{3,4,6,9},{3,4,7,8},{3,4,7,9},{3,4,8,9},{3,5,6,7},{3,5,6,8},{3,5,6,9},{3,5,7,8},{3,5,7,9},{3,5,8,9},{3,6,7,8},{3,6,7,9},{3,6,8,9},{3,7,8,9},{4,5,6,7},{4,5,6,8},{4,5,6,9},{4,5,7,8},{4,5,7,9},{4,5,8,9},{4,6,7,8},{4,6,7,9},{4,6,8,9},{4,7,8,9},{5,6,7,8},{5,6,7,9},{5,6,8,9},{5,7,8,9},{6,7,8,9} };
+
+int* generateKTuples(const int k) {
+	if (k == 1) {
+		return (int*)tuplesSize1;
+	}
+	else if (k == 2) {
+		return (int*)tuplesSize2;
+	}
+	else if (k == 3) {
+		return (int*)tuplesSize3;
+	}
+	else if (k == 4) {
+		return (int*)tuplesSize4;
+	}
+	else {
+		printf("Invalid value of k. Supported values are 1, 2, 3, or 4. Was given %d.\n", k);
 		return NULL;
 	}
-
-	// Calcul du nombre total de combinaisons
-	int totalCombinations = 1;
-	for (int i = 0; i < tupleSize; i++)
-	{
-		totalCombinations *= 9 - i;
-	}
-
-	// Vérification de la taille demandée par rapport au nombre total de combinaisons possibles
-	if (nbCombinations > totalCombinations)
-	{
-		fprintf(stderr, "Invalid number of combinations requested (%d)\n", nbCombinations);
-		return NULL;
-	}
-
-	// Allocation du tableau de résultats
-	int** tuples = (int**)malloc(nbCombinations * sizeof(int*));
-	for (int i = 0; i < nbCombinations; i++)
-	{
-		tuples[i] = (int*)malloc(nbCombinations * sizeof(int));
-	}
-
-	int tupleIndex = 0;
-
-	// Boucle pour générer les combinaisons
-	for (int i = 1; i <= 9 && tupleIndex < nbCombinations; i++)
-	{
-		if (tupleSize == 1)
-		{
-			tuples[tupleIndex][0] = i;
-			tupleIndex++;
-			continue;
-		}
-
-		for (int j = 2; j <= 9 && tupleIndex < nbCombinations; j++)
-		{
-			if (tupleSize == 2 && i != j)
-			{
-				tuples[tupleIndex][0] = i;
-				tuples[tupleIndex][1] = j;
-				tupleIndex++;
-				continue;
-			}
-
-			for (int k = 3; k <= 9 && tupleIndex < nbCombinations; k++)
-			{
-				if (tupleSize == 3 && i != j && j != k)
-				{
-					tuples[tupleIndex][0] = i;
-					tuples[tupleIndex][1] = j;
-					tuples[tupleIndex][2] = k;
-					tupleIndex++;
-					continue;
-				}
-
-				for (int l = 4; l <= 9 && tupleIndex < nbCombinations; l++)
-				{
-					if (tupleSize == 4 && i != j && j != k && k != l)
-					{
-						tuples[tupleIndex][0] = i;
-						tuples[tupleIndex][1] = j;
-						tuples[tupleIndex][2] = k;
-						tuples[tupleIndex][3] = l;
-						tupleIndex++;
-					}
-				}
-			}
-		}
-	}
-
-	return tuples;
 }
 
 void afficheBaton(unsigned char * baton)
